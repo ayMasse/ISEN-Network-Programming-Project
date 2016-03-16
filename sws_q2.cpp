91438c70797fbc5149f620497486cfa60ba99886
Aymeric MASSE <aym.masse@gamil.com>
2016-03-16 16:11:35 +0100

    added listen_fd

--- sws_q2.cpp
+++ sws_q2.cpp
@@ -1,19 +1,18 @@
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <sys/epoll.h>
-#include <sys/sendfile.h>
 
 #include "utility.h"
 #include "inet_socket.h"
 
 #define BUFFER_SIZE 10
-#define EPOLL_SIZE 10
-#define MAX_EVENTS 10
 
 int main()
 {
-    
+    int listen_fd = inetListen("8080", 5, NULL);
+    if (listen_fd == -1) {
+        errExit("inetListen");
+    }
+
+
 }
