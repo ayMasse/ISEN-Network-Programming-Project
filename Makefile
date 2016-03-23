addfffc94c1500552ea8899537aef1681d31be8b
Louis Gérard <louis.gerard@isen-lille.fr>
2016-03-23 15:48:21 +0100

    Debut q3

--- Makefile
+++ Makefile
@@ -6,6 +6,9 @@ RM=rm -f
 SRCS=inet_socket.cpp utility.cpp
 OBJS=inet_socket.o utility.o
 
+q3: $(OBJS)
+	$(CXX) $(CPPFLAGS) $(OBJS) -o sws_q3 sws_q3.cpp
+
 q2: $(OBJS)
 	$(CXX) $(CPPFLAGS) $(OBJS) -o sws_q2 sws_q2.cpp
 
---
+++ sws_q3.cpp
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include "utility.h"
+#include "inet_socket.h"
+
+#define BUF_SIZE 300
+
+int main()
+{
+    int listen_fd = inetListen("8080", 5, NULL);
+    if (listen_fd == -1){
+        errExit("inetListen");
+    }
+	
+	// Turn on non-blocking mode on the passive socket
+    int flags = fcntl(listen_fd, F_GETFL);
+    if (fcntl(listen_fd, F_SETFL, flags|O_NONBLOCK) == -1){
+        errExit("listen fd non block");
+	}
+	
+	for(;;){
+		int client_fd = accept(listen_fd,NULL,NULL);
+		if(client_fd == -1 && errno != EWOULDBLOCK){
+			errExit("accept");
+		}
+		if(client_fd != -1){
+			char buf[BUF_SIZE];
+			printf("Accept a new connection... \n");
+			int numRead = read(client_fd, buf, BUF_SIZE);
+			if(write(STDOUT_FILENO,buf,numRead) != numRead)
+			{
+				errExit("partial/failed write");
+			}
+			int fd = open("index.html",O_RDONLY);
+			if(fd < 0){
+				errExit("opening file failed");
+			}
+			numRead = read(fd,buf,50);
+			if(numRead != 0){
+				errExit("reading file failed");
+			}
+			write(client_fd,buf,numRead);
+			close(client_fd);
+			printf("Closed connection... \n");
+		}
+	}
+	close(listen_fd);
+}
