1e2220a8d218db1f73fe88ecf9b417324e644c6e
Louis Gérard <louis.gerard@isen-lille.fr>
2016-03-30 17:06:51 +0200

    patch q6

--- sws_q6.cpp
+++ sws_q6.cpp
@@ -49,8 +49,7 @@ int main()
 	struct epoll_event evlist[MAX_EVENTS];
 
 	for (;; ) {
-		int nb_fd_ready = epoll_wait(epfd, evlist, MAX_EVENTS, -1);
-
+		int nb_fd_ready = epoll_wait(epfd, evlist, MAX_EVENTS, 0);
 		if (nb_fd_ready == -1) {
 			if (errno == EINTR) // restart if interrupted by signal
 				continue;
@@ -58,33 +57,30 @@ int main()
 				errExit("epoll");
 		}
 		for (int i = 0; i < nb_fd_ready; ++i) {
-			int client_fd = accept(listen_fd, NULL, NULL);
-			if (client_fd == -1 && errno != EWOULDBLOCK)
-				errExit("accept");
-			if (client_fd != -1) {
+			int fd_ready = evlist[i].data.fd;
+			if (fd_ready == listen_fd) {
+				int client_fd = accept(listen_fd, NULL, NULL);
 				printf("Accept a new connection... \n");
 				flags = fcntl(client_fd, F_GETFL);
 				if (fcntl(client_fd, F_SETFL, flags | O_NONBLOCK) == -1) {
 					close(client_fd);
+					printf("Closed connection...\n");
 				} else {
 					ev.data.fd = client_fd;
 					if (epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &ev) == -1) {
 						close(client_fd);
-						printf("Closed connection... \n");
+						printf("Closed connection...\n");
 					}
 				}
 			}
-			int nb_fd = epoll_wait(epfd, evlist, MAX_EVENTS, 0);
-			for (int i = 0; i < nb_fd; i++) {
-				client_fd = evlist[i].data.fd;
+			else{
 				char buf[BUF_SIZE];
-				int numRead = read(client_fd, buf, BUF_SIZE);
+				int numRead = read(fd_ready, buf, BUF_SIZE);
+				if(numRead == 0){
+					close(fd_ready);
+				}
 				if (write(STDOUT_FILENO, buf, numRead) != numRead)
 					errExit("partial/failed write");
-				if (numRead == 0) { // The client closed the connection
-					printf("Closed connection...\n");
-					close(client_fd);
-				}
 				char *request = strtok(buf, " ");
 				request = strtok(NULL, " ");
 				char path[BUF_SIZE];
@@ -93,21 +89,21 @@ int main()
 				if (stat(path, &sb) == 0 && S_ISDIR(sb.st_mode)) {
 					char path2[BUF_SIZE];
 					strcpy(path2, path);
-					snprintf(path, BUF_SIZE, "%s/index.html", path2);
+					snprintf(path, BUF_SIZE, "%sindex.html", path2);
 					printf("PATH = %s\n", path);
 				}
 				int fd = open(path, O_RDONLY);
 				if (fd < 0) {
 					char headers[BUF_SIZE];
 					ssize_t nbBytes = snprintf(headers, BUF_SIZE, "HTTP/1.x 404 NOT FOUND\r\nConnection: keep-alive\r\n\r\n");
-					write(client_fd, headers, nbBytes);
+					write(fd_ready, headers, nbBytes);
 				} else {
 					struct stat stat_buf;
 					fstat(fd, &stat_buf);
 					char headers[BUF_SIZE];
 					ssize_t nbBytes = snprintf(headers, BUF_SIZE, "HTTP/1.x 200 OK\r\nContent-Type: text/html;\r\nContent-Length: %i\r\ncharset=UTF-8\r\nConnection: keep-alive\r\n\r\n", stat_buf.st_size);
-					write(client_fd, headers, nbBytes);
-					sendfile(client_fd, fd, 0, stat_buf.st_size);
+					write(fd_ready, headers, nbBytes);
+					sendfile(fd_ready, fd, 0, stat_buf.st_size);
 				}
 			}
 		}
